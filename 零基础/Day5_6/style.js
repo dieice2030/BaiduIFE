f1ad3a839392cabe3569ad94725560aa9375e9f8
dieice2030 <dieice2030@163.com>
2018-06-08 16:51:56 +0800

    modify button

--- 零基础/Day5_6/style.js
+++ 零基础/Day5_6/style.js
@@ -4,10 +4,8 @@ var head = document.getElementsByTagName('head')[0];
 
 function checkStyle(i, button) {
     var link = document.getElementsByTagName('link')[0];
-    var newLink = document.createElement('link');
     button[i].addEventListener('click', function () {
-        link.type = 'text/css';
-        link.rel = 'stylesheet';
+        link.removeAttribute('href')
         link.href = 'style_' + (i + 1) + '.css';
         
     });
