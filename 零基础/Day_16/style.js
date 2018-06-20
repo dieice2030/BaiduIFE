0b6561d0c4c6238f8245cf559cf577585aa92416
dieice2030 <dieice2030@163.com>
2018-06-20 22:05:59 +0800

    day16

---
+++ 零基础/Day_16/style.js
@@ -0,0 +1,11 @@
+var hello=document.getElementById('hello');
+var hobby=document.getElementById('hobby');
+
+hello.onclick=function(){
+    console.log('你好，我是dieice2030');
+    alert("记得打开开发者工具哦！");
+}
+hobby.onclick=function(){
+    console.log('我喜欢搓炉石、打LOL，最近刚拿到竞技场12胜钥匙呢^_^');
+    alert("记得打开开发者工具哦！");
+}
